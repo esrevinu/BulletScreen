8e691a49f212d796e516aaab225be726d0c7093e
Li Zhenyu <icytornado@163.com>
2016-12-16 16:00:29 +0800

    Text format can't be used for HTML?

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# 实时弹幕系统
-## Installing
+# Bullet screen
+## Install
 ```
 npm install
 bower install
+npm start
 ```
+## Instructions of use
+- Access the /emit to emit a new "bullet"
+- Access the / to check the result
+
+## 使用WebSocket的原因
+弹幕发送的同时希望能显示在此时所有终端用户屏幕前,否则只有自己能看到
+
+## TODO
+1. 把发送弹幕功能集成在同一个页面
+2. 把监听弹幕功能
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,4 +15,69 @@ window.addEventListener('load', function () {
         var danmaku = JSON.parse(msg);
         CM.send(danmaku);
     });
+
+    // cmtController();
 });
+
+function getCmtDataList() {
+    var cmtArr = [];
+
+    // 可以使用jsonp获取服务器的字幕数据
+    /*$.ajax({
+     type : 'GET',
+     url : 'http://192.168.9.67/test.php',
+     dataType : 'jsonp',
+     data : {sid : 100},
+     success : function(data) {
+     cmtArr = data.dataList;
+
+     if (cmtArr && cmtArr.length > 0) {
+     sendMsg(cmtArr);
+     }
+     }
+     });*/
+
+    // 测试数据
+    cmtArr = [
+        {"text":"大家期待什么新品啊", "bgColor":"#424448", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"会有什么惊喜吗？", "bgColor":"#424448", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"等待中。。", "bgColor":"#23b28b", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"会有什么新产品呢？", "bgColor":"#424448", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"定时执行", "bgColor":"#23b28b", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"1123333446红咖喱的非农房价", "bgColor":"#ec4262", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"测试接口发评论00", "bgColor":"#ec4262", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"测试接口发评论00", "bgColor":"#3dbbc0", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"啊啊啊啊啊啊啊哦哦哦诶IEIE恩家报表出具", "bgColor":"#ec4262", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"},
+        {"text":"的方式的方法反反复复反复反复", "bgColor":"#23b28b", "icon":"http://face.weiphone.net/data/avatar/000/15/31/95_avatar_big.jpg"}
+    ];
+
+    sendMsg(cmtArr);
+}
+
+
+function sendMsg(cmtArr) {
+
+    for (var i=0; i<cmtArr.length; i++) {
+        var cmtItem = cmtArr[i],
+            iconStr = '';
+
+        if (cmtItem.icon && cmtItem.icon.length > 0) {
+            iconStr = '<span class="icon"><img src="'+ cmtItem.icon +'"></span>';
+        }
+
+        // 字幕的节点内容
+        cmtItem.text = iconStr + cmtItem.text;
+        cmtItem.mode = 8;
+        cmtItem.dur = Math.floor(Math.random()*4000 + 4000);
+
+        CM.send(cmtItem);
+    }
+}
+
+function cmtController() {
+    getCmtDataList();
+
+    setTimeout(function(){
+        cmtController();
+    }, 5000);
+}
