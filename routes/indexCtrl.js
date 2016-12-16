53633226a596345e191d768634caadfc368041f8
Li Zhenyu <icytornado@163.com>
2016-12-16 17:05:47 +0800

    Integrated the emit function into index page

--- routes/indexCtrl.js
+++ routes/indexCtrl.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/',function(req,res,next){
-    res.render('index',{title:"danmaku"});
+    var config = JSON.parse(fs.readFileSync(__dirname + './../public/jsons/config.json'));
+    res.render('index', {
+        title: 'Bullet Screen',
+        sizes: config.sizes,
+        modes: config.modes,
+        colors: config.colors,
+        durations: config.durations,
+        inits: config.inits
+    });
 });
 
 module.exports = router;
